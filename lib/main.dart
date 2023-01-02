c7d6445042d0fea0339a673654b089750382a647
murillo <tumusx1@gmail.com>
2023-01-02 15:30:43 -0300

    create card game

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+const String eldenRingName = "Elden Ring";
+const String fifa22 =
+    "https://image.api.playstation.com/vulcan/img/rnd/202111/0822/Lu3i2jWOkqTXwzYASl4aIQDQ.jpg";
+const String fifa22Name = "FIFA 22";
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,6 +14,45 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Scaffold(backgroundColor: Colors.teal));
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://ovicio.com.br/wp-content/uploads/2022/11/20221114-ovicio-elden-ring-capa-555x555.webp"),
+                  radius: 50,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                eldenRingName,
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                  height: 20, width: 150, child: Divider(color: Colors.white)),
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(fifa22),
+                  radius: 50,
+                  backgroundColor: Colors.cyan,
+                ),
+              ),
+              SizedBox(height: 20),
+              Text(
+                fifa22Name,
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
